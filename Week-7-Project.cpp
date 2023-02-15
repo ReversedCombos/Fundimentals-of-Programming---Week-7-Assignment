a785b876d4356c696f4a628a5519db2291b7d26f
Haydon Pool <hpool2004@gmail.com>
2023-02-15 07:21:12 -0600

    Added the basics of the file along with error checking for the file created

--- Week-7-Project.cpp
+++ Week-7-Project.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+//Does error checking for the fileName - If they are any of those characters then the file wont be made
+bool StringErrorCheck(string fileName)
+{
+    int fileNameLen = fileName.length();
+
+    for (int i = 0; i <= fileNameLen; i++) 
+    {
+        if (fileName[i] == '*' || fileName[i] == '.' || fileName[i] == '"' || fileName[i] == '/' || fileName[i] == '\\' || fileName[i] == ' [ ' || fileName[i] == ']' || fileName[i] == ':' || fileName[i] == ';' || fileName[i] == '|' || fileName[i] == ',')
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
-    cout << "Hello World!\n";
+    //Varables
+    string fileName;
+
+
+    //Setting up the outFile refrence
+    ofstream outFile;
+
+    while (true)
+    {
+        //Prompting the filename;
+        cout << "Input File Name : ";
+
+        //Outputting the input of the entire line into the fileName variable
+        getline(cin, fileName);
+
+        if (StringErrorCheck(fileName))
+        {
+            break;
+        }
+        cout << "Please try again without (*) (.) (\") (/) (\\) ({) (}) (:) (;) (|) (,) \n";
+    }
+    
+    //Opening the file with the fileName
+    outFile.open(fileName);
 }
