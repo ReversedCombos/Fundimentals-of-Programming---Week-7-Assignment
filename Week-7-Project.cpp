35a0a0466edcbdc89657658e9db83eba28720f44
ReversedCombos <49067277+ReversedCombos@users.noreply.github.com>
2023-02-16 09:42:01 -0600

    Added outputs for the end user

--- Week-7-Project.cpp
+++ Week-7-Project.cpp
@@ -8,95 +8,101 @@ using namespace std;
 //Does error checking for the fileName - If they are any of those characters then the file won't be made
 bool StringErrorCheck(string fileName)
 {
-    int fileNameLen = fileName.length();
+	int fileNameLen = fileName.length();
 
-    for (int i = 0; i <= fileNameLen; i++) 
-    {
-        if (fileName[i] == '*' || fileName[i] == '.' || fileName[i] == '"' || fileName[i] == '/' || fileName[i] == '\\' || fileName[i] == ' [ ' || fileName[i] == ']' || fileName[i] == ':' || fileName[i] == ';' || fileName[i] == '|' || fileName[i] == ',')
-        {
-            return false;
-        }
-    }
-    return true;
+	for (int i = 0; i <= fileNameLen; i++)
+	{
+		if (fileName[i] == '*' || fileName[i] == '.' || fileName[i] == '"' || fileName[i] == '/' || fileName[i] == '\\' || fileName[i] == ' [ ' || fileName[i] == ']' || fileName[i] == ':' || fileName[i] == ';' || fileName[i] == '|' || fileName[i] == ',')
+		{
+			return false;
+		}
+	}
+	return true;
 }
 
 int main()
 {
-    //Varables
-    string fileName;
-    double subTotal;
-    double tipAmount;
-    double taxRate;
-    double total;
-    int numInParty;
+	//Varables
+	string fileName;
+	double subTotal;
+	double tipAmount;
+	double taxRate;
+	double total;
+	int numInParty;
 
-    //Setting up the outFile refrence
-    ofstream outFile;
+	//Setting up the outFile refrence
+	ofstream outFile;
 
-    while (true)
-    {
-        //Prompting the filename;
-        cout << "Input File Name : ";
+	cout << "Welcome to the Receipt Checker 7000!" << endl;
+	cout << "Enter the receipt details and we will recommend how to split the tip and total!" << endl << endl;
 
-        //Outputting the input of the entire line into the fileName variable
-        getline(cin, fileName);
+	while (true)
+	{
+		//Prompting the filename;
+		cout << "Input the output file name : ";
 
-        if (StringErrorCheck(fileName))
-        {
-            break;
-        }
-        cout << "Please try again without (*) (.) (\") (/) (\\) ({) (}) (:) (;) (|) (,) \n";
-    }
-    
-    //Opening the file with the fileName
-    outFile.open(fileName);
-    
-    //Setting up a while function to repeat inputs if there is an error
-    while(true)
-    {
-        cout << "Enter the check amount : ";
-        cin >> subTotal;
-        cout << endl;
-        
-        cout << "Enter the tip amount : ";
-        cin >> tipAmount;
-        cout << endl;
-        
-        cout << "Enter the tax rate : ";
-        cin >> taxRate;
-        cout << endl;
-        
-        cout << "Enter the number of people in your party : ";
-        cin >> numInParty;
-        cout << endl;
-        
-        //Due to being put into a FailState, it will skip to the last input and set it to 0
-        //If the last input is 0, then it has been put into a fail state and needs to be reset.
-        if(numInParty == 0)
-        {
-            cout << "Make sure to enter the correct values" << endl;
-            
-            //Clears the input stream of the fail state
-            cin.clear();
-            
-            //Ignores the rest of the input
-            cin.ignore(200, '\n');
-        }
-        else
-        {
-            //If no error can be found, then it will break out of the while loop and continue
-            break;
-        }
-    }
-    
-    total = subTotal + (subTotal * taxRate);
-    
-    outFile << "Receipt : " << endl << endl;
-    outFile << "Subtotal : " << subTotal << endl;
-    outFile << "Tax Rate : " << taxRate << endl;
-    outFile << "Total : " << total << endl;
-    outFile << "Tip Amount : " << tipAmount << endl;
-    outFile << "Number of people in party : " << numInParty << endl;
-    outFile << "Recommended split of check : " << total / numInParty << endl;
-    outFile << "Recommended split of tip : " << tipAmount / numInParty;
+		//Outputting the input of the entire line into the fileName variable
+		getline(cin, fileName);
+
+		if (StringErrorCheck(fileName))
+		{
+			break;
+		}
+		cout << "Please try again without (*) (.) (\") (/) (\\) ({) (}) (:) (;) (|) (,) \n";
+	}
+
+	//Opening the file with the fileName
+	outFile.open(fileName);
+
+	//Setting up a while function to repeat inputs if there is an error
+	while (true)
+	{
+		cout << "Enter the check amount : ";
+		cin >> subTotal;
+		cout << endl;
+
+		cout << "Enter the tip amount : ";
+		cin >> tipAmount;
+		cout << endl;
+
+		cout << "Enter the tax rate : ";
+		cin >> taxRate;
+		cout << endl;
+
+		cout << "Enter the number of people in your party : ";
+		cin >> numInParty;
+		cout << endl;
+
+		//Due to being put into a FailState, it will skip to the last input and set it to 0
+		//If the last input is 0, then it has been put into a fail state and needs to be reset.
+		if (numInParty == 0)
+		{
+			cout << "Make sure to enter the correct values" << endl;
+
+			//Clears the input stream of the fail state
+			cin.clear();
+
+			//Ignores the rest of the input
+			cin.ignore(200, '\n');
+		}
+		else
+		{
+			//If no error can be found, then it will break out of the while loop and continue
+			break;
+		}
+	}
+
+	cout << "Thank you for using the Receipt Checker 7000!" << endl;
+	cout << "Receipt Checker 8000 Release Date : February 16, 2043" << endl;
+
+	total = subTotal + (subTotal * taxRate);
+
+	outFile << "Receipt : " << endl << endl;
+	outFile << "Subtotal : " << subTotal << endl;
+	outFile << "Tax Rate : " << taxRate << endl;
+	outFile << "Total : " << total << endl;
+	outFile << "Tip Amount : " << tipAmount << endl;
+	outFile << "Number of people in party : " << numInParty << endl;
+	outFile << "Recommended split of check : " << total / numInParty << endl;
+	outFile << "Recommended split of tip : " << tipAmount / numInParty;
 }
